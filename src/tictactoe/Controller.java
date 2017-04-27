d83e484e24be58dc556107bc6e97588f61dade28
mosta <mosta@192.168.0.241>
2017-04-27 20:57:21 +0200

    Update

--- src/tictactoe/Controller.java
+++ src/tictactoe/Controller.java
@@ -1,46 +1,28 @@
 package tictactoe;
 
-import javafx.scene.input.MouseButton;
-import tictactoe.ServiceLocator;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class Controller {
-	
+
 	ServiceLocator serviceLocator;
 	private Client client;
 	private View view;
-	
 
 	public Controller(Client client, View view) {
 		this.client = client;
 		this.view = view;
-		
-		
-		//create a tile object as inner class that the controller can
-		//do the drawing
-		//the logger checks if there is an object
-		
-		serviceLocator = ServiceLocator.getServiceLocator();
-		Tile tilo = view.getTile();
-		serviceLocator.getLogger().info("tile ist:" +tilo);
-//		serviceLocator.getLogger().info("tile ist:" +view.getTile());		
-//		
-//		
-//		//register ourselves to listen for mouse clicks
-//		view.tile.setOnMouseClicked(event ->{
-//			if(event.getButton() == MouseButton.PRIMARY){
-//				view.tile.drawX();
-//			}
-//			else if (event.getButton()== MouseButton.SECONDARY){
-//				view.tile.drawO();
-//			}	
-//		});
-			
-	
-		
-	//  serviceLocator = ServiceLocator.getServiceLocator();
-	//	serviceLocator = getLogger().info("Application controller initialized");	
-		
+
+		view.button.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				client.addSymbol();
+				String newText = Integer.toString(client.getValue());
+
+			}
+		});
+
 	}
-	
+
 }
--- src/tictactoe/View.java
+++ src/tictactoe/View.java
@@ -1,133 +1,87 @@
 package tictactoe;
 
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
-import tictactoe.ServiceLocator;
 
 public class View {
 
-	
+	private static final double HEIGHT = 600;
+	private static final double WIDTH = 600;
+
 	private Stage stage;
 	private Client client;
-	private Scene scene;
+	private Scene scene1;
 	private ServiceLocator serviceLocator;
-	protected Tile tile;
+	protected Button button;
 
-	
 	public View(Stage stage, Client client) {
 		this.stage = stage;
 		this.client = client;
-	
-		
+
 		stage.setTitle("Tic Tac Toe");
-		
-		scene = create_GUI();
+
+		GridPane root = new GridPane();
+		root.setPrefSize(WIDTH, HEIGHT);
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+
+				Button button = new Button();
+				button.setMaxWidth(Double.MAX_VALUE);
+				button.setMaxHeight(Double.MAX_VALUE);
+
+				// button id's are set
+				if (i == 0) {
+					button.setId(String.valueOf(j + 1));
+				}
+
+				if (i == 1) {
+					button.setId(String.valueOf(j + 1 + 3));
+				}
+
+				if (i == 2) {
+					button.setId(String.valueOf(j + 1 + 6));
+				}
+
+				root.add(button, i, j);
+				serviceLocator = ServiceLocator.getServiceLocator();
+				serviceLocator.getLogger().info(button.getId());
+
+				// buttons fill max width of GridPane
+				if (i == 0) {
+					ColumnConstraints column = new ColumnConstraints();
+					column.setPercentWidth(33);
+					root.getColumnConstraints().add(column);
+				}
+			}
+
+			// buttons fill max height of GridPane
+			RowConstraints rowConstraints = new RowConstraints();
+			rowConstraints.setPercentHeight(33);
+			root.getRowConstraints().add(rowConstraints);
+
+		}
+
+		Scene scene = new Scene(root);
 		stage.setScene(scene);
-		
-		
-		
-		
+
 		serviceLocator = ServiceLocator.getServiceLocator();
 		serviceLocator.getLogger().info("Application view initialized");
-		
+
 	}
 
-
-
-	
-	protected Scene create_GUI(){
-		
-		Pane root = new Pane();
-		root.setPrefSize(600, 600);
-		
-		for (int i = 0; i < 3; i++){
-			for (int j = 0; j <3; j++){
-				Tile tile = new Tile();
-				tile.setTranslateX(j*200);
-				tile.setTranslateY(i*200);
-				
-				root.getChildren().add(tile);
-				
-				
-			}
-		}
-		
-		Scene scene = new Scene(root);
-		return scene;
-		
-		
-	}
-	
-//	public class Tile extends StackPane {
-//		
-//		private Text text = new Text();
-//		private Tile tile;
-//		
-//		
-//		public Tile(){	
-//			
-//			Rectangle border = new Rectangle(200,200);
-//			border.setFill(null);
-//			border.setStroke(Color.BLACK);
-//			
-//			text.setFont(Font.font(72));
-//			
-//			setAlignment(Pos.CENTER);
-//			getChildren().addAll(border, text);
-//			
-//			setOnMouseClicked(event ->{
-//				if(event.getButton() == MouseButton.PRIMARY){
-//					drawX();
-//				}
-//				else if (event.getButton()== MouseButton.SECONDARY){
-//					drawO();
-//				}	
-//			});
-//			
-//			
-//			
-//		}
-//		
-//		protected void drawX(){
-//			text.setText("X");
-//		}
-//		
-//		protected void drawO(){
-//			text.setText("O");
-//		}
-//		
-//	
-//
-//}
-	
-	
-	
 	public void start() {
 		stage.show();
-		
+
 	}
 
 	public void stop() {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	public Tile getTile(){
-		return tile;
-	}
-	
-	public void setTile (Tile tile){
-		this.tile = tile;
-	}
 
+	}
 
 }
